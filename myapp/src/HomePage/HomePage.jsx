3e9cd956f8d8e7ee1517e245d8c1eb5ca81a5b0e
adarshgithubit <104212058+adarshgithubit@users.noreply.github.com>
2023-01-23 01:40:54 +0530

    change

--- myapp/src/HomePage/HomePage.jsx
+++ myapp/src/HomePage/HomePage.jsx
@@ -1,17 +1,75 @@
 import React from 'react'
 import "./HomePage.css"
+import  { useEffect, useRef } from "react";
+ 
+import Webcam from "react-webcam";
 
 const HomePage = () => {
+  let videoRef = useRef(null);
+ 
+  let photoRef = useRef(null)
+ 
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+ 
+  const takePicture = () => {
+    const width = 400
+    const height = width / (16 / 9)
+    
+    let video = videoRef.current
+ 
+    let photo = photoRef.current
+ 
+    photo.width = width
+ 
+    photo.height = height
+ 
+    let ctx = photo.getContext('2d')
+ 
+    ctx.drawImage(video, 0, 0, width, height)
+    
+  }
+ 
+  const clearImage = () => {
+    let photo = photoRef.current
+ 
+    let ctx = photo.getContext('2d')
+ 
+    ctx.clearRect(0,0,photo.width,photo.height)
+  }
+ 
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+ 
+
   return (
     <>
-    <div className='container'>
-    <div className='homepage'>
-        <h1>Hello Users Can You are ready to take a Snap</h1>
-        <div className='button'>Logout</div>
+     <div className="container">
+      <h1 className="text-center">Camera Selfie App in React</h1>
+ 
+      <video ref={videoRef} className="container"></video>
+ 
+      <button onClick={takePicture} className="btn btn-danger container">Take Picture</button>
+ 
+      <canvas className="container" ref={photoRef}></canvas>
+ 
+      <button onClick={clearImage} className="btn btn-primary container">Clear Image</button>
+ 
+      <br/><br/>
     </div>
-
-    </div>
-    
     </>
   )
 }
